161c3cc7b9c4d9f1d14960e44822246b69ac3460
Pranmya Joshi <pranmya04@gmail.com>
2026-08-23 01:20:48 +0530

    Revert "style: set font weight to 500 (medium) for perfect visual thickness match with quote"
    This reverts commit 3b9000e88ebc51a345a70805ec7883aea3c165e6.

--- api/wallpaper.js
+++ api/wallpaper.js
@@ -5,8 +5,6 @@ const { WORDS, QUOTES } = require('../data.js');
 try {
     registerFont(path.join(__dirname, '..', 'fonts', 'CourierPrime-Bold.ttf'), { family: 'Courier Prime', weight: 'bold' });
     registerFont(path.join(__dirname, '..', 'fonts', 'Outfit.ttf'), { family: 'Outfit', weight: 'normal' });
-    registerFont(path.join(__dirname, '..', 'fonts', 'Outfit-Medium.ttf'), { family: 'Outfit', weight: '500' });
-    registerFont(path.join(__dirname, '..', 'fonts', 'Outfit-SemiBold.ttf'), { family: 'Outfit', weight: '600' });
     registerFont(path.join(__dirname, '..', 'fonts', 'Outfit-Bold.ttf'), { family: 'Outfit', weight: 'bold' });
     registerFont(path.join(__dirname, '..', 'fonts', 'Lora-Italic.ttf'), { family: 'Lora', style: 'italic' });
 } catch (e) {
@@ -85,7 +83,7 @@ module.exports = async function (req, res) {
         
         // 2. Date String
         const dateY = wordY + 80 * scale;
-        ctx.font = `500 ${34 * scale}px "Outfit", sans-serif`;
+        ctx.font = `400 ${34 * scale}px "Outfit", sans-serif`;
         ctx.fillStyle = '#ffffff'; // Changed to pure white
         ctx.fillText(dateString, centerX, dateY);
         
@@ -168,7 +166,7 @@ module.exports = async function (req, res) {
         
         // Author
         const authorY = currentQuoteY + 30 * scale;
-        ctx.font = `500 ${34 * scale}px "Outfit", sans-serif`;
+        ctx.font = `400 ${34 * scale}px "Outfit", sans-serif`;
         ctx.fillStyle = theme.accent;
         ctx.fillText(authorText, centerX, authorY);
         
